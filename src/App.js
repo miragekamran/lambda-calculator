41e1befe30a422b7498f73e167e6da6a8d663121
Mirage Kamran <55200336+miragekamran@users.noreply.github.com>
2020-01-09 22:16:22 -0800

    project updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
 import "./App.css";
+import Specials from './components/ButtonComponents/SpecialButtons/SpecialButton';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -16,6 +19,9 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Numbers/>
+      <Operators/>
+      <Specials/>
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  console.log(props, 'special buttons')
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{props.command}</button>
     </>
   );
 };
+
+export default SpecialButton;
